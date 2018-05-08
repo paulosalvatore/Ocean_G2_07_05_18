20650598786a614aa089071d98063a5b1888f8e9
Paulo Salvatore <salvatoregames@gmail.com>
2018-05-07 21:32:23 -0300

    Initial commit

---
+++ Assets/Scripts/Jogador.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Jogador : MonoBehaviour
+{
+	public Rigidbody2D rb;
+
+	public float forcaPulo = 300f;
+
+	public Text pontosText;
+	private int pontos = 0;
+
+	// Use this for initialization
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Pular();
+		}
+	}
+
+	private void Pular()
+	{
+		rb.velocity = Vector2.zero;
+		rb.AddForce(Vector2.up * forcaPulo);
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.CompareTag("ColisorPontos"))
+		{
+			pontos = pontos + 1;
+			pontosText.text = "Pontos: " + pontos;
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("Pedras"))
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+	}
+}
---
+++ Assets/Scripts/Pedras.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Pedras : MonoBehaviour
+{
+	public float velocidade = 3f;
+	public float delay = 10f;
+
+	// Use this for initialization
+	void Start()
+	{
+		Destroy(gameObject, delay);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.Translate(Vector3.left * velocidade * Time.deltaTime);
+	}
+}
